9058362ad6a716f8e21f8f7390a071925d5af28d
lucasjkr <lucaskrupinski@me.com>
2025-01-05 12:20:11 -0500

    commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='SimpleGraphApi',
+    version='0.0.1',
+    packages=['msgraph'],
+    package_dir={'msgraph': 'src/msgraph'},
+    url='',
+    license='',
+    author='lucasjkr',
+    author_email='',
+    description='',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+    ]
+)
